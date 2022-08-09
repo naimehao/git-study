e3fba092d0fd4cfca3ebac1a33476733c4e4fdfb
naimehao <naimehao@outlook.com>
2022-08-09 17:11:02 +0800

    func_a1 new file

---
+++ test2.js
@@ -0,0 +1,14 @@
+let a = 1
+console.log(a);
+console.log("feature1 commit")
+console.log("feature1 new func")
+
+function a1() {
+
+    console.log("a1 func coding...2")
+
+}
+
+function branch_main() {
+    console.log("branch_main1")
+}
